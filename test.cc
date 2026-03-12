591e416e43bf9fb3e404a0a9c9b15a8edf8723cf
Gracy Cabrera <gracycabrera@mac.mynetworksettings.com>
2026-03-12 11:49:03 -0700

    hackalong

--- test.cc
+++ test.cc
@@ -1,2 +1,23 @@
 // Name: Grecia Cabrera
 // ID: 821201902
+#include <string>
+#include <iostream>
+#include <cstdint>
+
+void double_it(int& x){
+    x = x * 2;
+    std::cout << "x is " << x << "\n";
+}
+
+void double_it_pointer (int x){
+    std::uintptr_t y = reinterpret_cast<std::uintptr_t>(x);
+    *x = *x * 2;
+    std::cout << "x is " << x << "\n";
+}
+
+int main() {
+    int value [] = {9, 7, 13, 19, 23};
+    std::cout << "value is " << value << "\n";
+    double_it_pointer(&value);
+    std::cout << "value is " << value << "\n";
+}
